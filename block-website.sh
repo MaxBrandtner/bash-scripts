44bc33d2fb63d9155851f9e8136cdc4dd333b479
MaxBrandtner <maximilian.immanuel.brandtner@protonmail.com>
2023-08-03 09:49:54 +0200

    reformatting

--- block-website.sh
+++ block-website.sh
@@ -1,34 +1,17 @@
 #blocks website in the hostfile
 #$1 = website name
-url=$1
 
-if [ $# -eq 0 ]
-then
-	echo "no argument provided"
-	exit
-fi
+[ $1 ] || exit
+
+[ $(whoami) != "root" ] && pkexec "$(pwd)/block-website.sh" "$1"
 
 
-first_characters_input=`echo $1 | cut -c1-4`
-echo $first_characters_input
-if [ $first_characters_input == 'www.' ]
-then
-	echo $url
-	url=`echo "$url" | awk '{ print substr( $0, 5 ) }'`
-	echo $url	
-fi
+url=$( (echo "$1" | grep '^www.' | awk '{print substr( $0, 5 )}') || (echo "$1") )
 
-if [ whoami != 'root' ]
-then
-	exec= `echo "$(pwd)/block-website.sh"`
-	pkexec echo "$exec $1"
-else
 
-echo " "              	>> /etc/hosts
-echo 0.0.0.0   www.$url	>> /etc/hosts
-echo 0.0.0.0   $url    	>> /etc/hosts
-echo ::0       www.$url	>> /etc/hosts
-echo ::0       $url   	>> /etc/hosts
-echo " "	      	>> /etc/hosts
-fi
-
+echo "\n
+echo 0.0.0.0   www.$url
+echo 0.0.0.0   $url
+echo ::0       www.$url
+echo ::0       $url 
+" >> /etc/hosts
